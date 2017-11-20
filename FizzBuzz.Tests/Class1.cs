1ed0586f1850e83b0cc4cb5dac2ea6bf31fa6a9b
tomorrowtraxx <tomorrowtraxx@gmail.com>
2017-11-20 20:01:59 +0100

    Add project files.

---
+++ FizzBuzz.Tests/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        private Game _game;
+
+        public FizzBuzzTests()
+        {
+            _game = new Game();
+        }
+
+        [Fact]
+        public void TestIsNumberOne()
+        {
+            Assert.Equal("1", _game.Next());
+        }
+
+        [Fact]
+        public void TestIsNumberTwo()
+        {
+            _game.Next();
+
+            Assert.Equal("2", _game.Next());
+        }
+
+        [Fact]
+        public void TestIsFizz()
+        {
+            _game.Next();
+            _game.Next();
+
+            Assert.Equal("Fizz", _game.Next());
+        }
+
+        [Fact]
+        public void TestIsNumberFour()
+        {
+            _game.Next();
+            _game.Next();
+            _game.Next();
+
+            Assert.Equal("4", _game.Next());
+        }
+
+        [Fact]
+        public void TestIsBuzz()
+        {
+            _game.Next();
+            _game.Next();
+            _game.Next();
+            _game.Next();
+
+            Assert.Equal("Buzz", _game.Next());
+        }
+    }
+}
---
+++ FizzBuzz/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        private int _number;
+        public Game()
+        {
+            _number = 0;
+        }
+
+        public IEnumerable<char> Next()
+        {
+            _number += 1;
+
+            if (_number == 3) return "Fizz";
+
+            if (_number == 5) return "Buzz";
+
+            return _number.ToString();
+        }
+    }
+}
